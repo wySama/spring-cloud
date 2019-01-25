4d77bf39afc5ff24bcc23ecc13ce63b8989b610e
wySama <1921067947@qq.com>
2019-01-25 16:34:21 +0800

    spring-cloud-zuul commit

---
+++ spring-cloud-zuul/eureka-service-producer/src/test/java/com/wy/eurekaserviceproducer/EurekaServiceProducerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wy.eurekaserviceproducer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServiceProducerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
